e3887b1b38cf6249284c61df0d2abb86a6fb70fd
Stève Commer <steve.commer@laposte.net>
2024-02-08 09:15:41 +0100

    Initial commit

---
+++ src/main/java/xyz/strashi/ruestarmel/RuestarmelApplication.java
@@ -0,0 +1,13 @@
+package xyz.strashi.ruestarmel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuestarmelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuestarmelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/strashi/ruestarmel/RuestarmelApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.strashi.ruestarmel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuestarmelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
